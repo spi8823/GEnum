e9ed5bcf85a4711fe15631e1c4a8966cfe2b266e
spi8823 <shin.8823@outlook.jp>
2025-02-07 22:50:29 +0900

    機能ごとにCSファイルを分離

---
+++ GEnum/GEnumGenerator.Flags.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GEnum
+{
+    [AttributeUsage(AttributeTargets.Enum)]
+    public class FlagsExtensionsAttribute : Attribute { }
+
+    public partial class GEnumGenerator
+    {
+        private static void EmitFlagsExtensions(SourceProductionContext context, GeneratorAttributeSyntaxContext source)
+        {
+            var typeSymbol = (INamedTypeSymbol)source.TargetSymbol;
+            var typeName = typeSymbol.Name;
+
+            var code = $$"""
+                {{GetNameSpaceExpression(typeSymbol)}}
+
+                public static partial class {{typeName}}Extensions
+                {
+                    public static bool Contains(this {{typeName}} target, {{typeName}} value)
+                    {
+                        return (target & value) == value;
+                    }
+
+                    public static void Add(ref this {{typeName}} target, {{typeName}} value)
+                    {
+                        target |= value;
+                    }
+
+                    public static void Remove(ref this {{typeName}} target, {{typeName}} value)
+                    {
+                        target &= ~value;
+                    }
+
+                    public static void Clear(ref this {{typeName}} target)
+                    {
+                        target = ({{typeName}})0;
+                    }
+                }
+                """;
+
+            var fileName = $"{typeName}Extensions.Flags.g.cs";
+            context.AddSource(fileName, code);
+        }
+    }
+}
---
+++ GEnum/GEnumGenerator.Matching.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GEnum
+{
+
+    [AttributeUsage(AttributeTargets.Enum)]
+    public class MatchingExtensionsAttribute : Attribute { }
+    public partial class GEnumGenerator
+    {
+        private static void EmitMatchingExtensions(SourceProductionContext context, GeneratorAttributeSyntaxContext source)
+        {
+            var typeSymbol = (INamedTypeSymbol)source.TargetSymbol;
+            var typeName = typeSymbol.Name;
+            var valueNames = typeSymbol.MemberNames;
+            var methodBuilder = new StringBuilder();
+            foreach (var valueName in valueNames)
+            {
+                methodBuilder.AppendLine($$"""
+                        public static bool Is{{valueName}}(this {{typeName}} target) => target == {{typeName}}.{{valueName}};
+                    """);
+            }
+
+            var code = $$"""
+                {{GetNameSpaceExpression(typeSymbol)}}
+
+                public static partial class {{typeName}}Extensions
+                {
+                {{methodBuilder.ToString()}}
+                }
+                """;
+
+            var fileName = $"{typeName}Extensions.Matching.g.cs";
+            context.AddSource(fileName, code);
+        }
+    }
+}
